31ddadedca16bb36d6138aefa7bf92457bac28b9
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-11-02 22:23:04 +0000

    Add solution for 11044

---
+++ archive/Volume 110/Task11044.java
@@ -0,0 +1,10 @@
+package main;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Task11044 {
+    public void solve(int testNumber, Scanner in, PrintWriter out) {
+      out.println((in.nextInt()/3)*(in.nextInt()/3));
+    }
+}
