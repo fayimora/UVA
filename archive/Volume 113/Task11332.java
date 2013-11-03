f1b313431a9b1497e29edccfaced612e2d085304
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-11-03 14:58:14 +0000

    Add solution for 11332

---
+++ archive/Volume 113/Task11332.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.Scanner;
+import java.io.PrintWriter;
+
+public class Task11332 {
+  public void solve(int testNumber, Scanner in, PrintWriter out) {
+    //Taking the modulo 9 of any number will return the sum of digits
+    // of that number until a single digit number is obtained.
+    while(in.hasNextInt()){
+      int n = in.nextInt();
+      if(n ==0) break;
+      out.println((n%9 == 0 && n != 0) ? 9 : n%9);
+    }
+  }
+}
